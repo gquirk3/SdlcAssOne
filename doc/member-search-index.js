42b1ba7d54a83e00af69057f2947752c3095fed0
Graham Quirk <gquirk@valenciacollege.edu>
2022-10-30 17:46:11 -0400

    added javadoc comments

---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"SdlcAssOne","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"SdlcAssOne","l":"SdlcAssOne()","u":"%3Cinit%3E()"}];updateSearchResults();
--- src/SdlcAssOne.java
+++ src/SdlcAssOne.java
@@ -1,17 +1,31 @@
 import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
+	/**
+	 * Class assignment that retrieves a poem from a specified website.  Jsoup is used to get the content of the poem, the author and the title only 
+	 * and input into a string.  The program then determines the word count for each word in the poem and outputs to the console the top 20 words
+	 * and their frequency.
+	 * @author ha33a
+	 * 
+	 */
 public class SdlcAssOne {
+	/**
+	 * This is the main method.
+	 * @param args  Strings passed into the main
+	 */
 
 	public static void main(String[] args)
 	{
 		
-		String text = "";  // variable to store the poem
+ 		String text = "";  // variable to store the poem
 		
 		try
 		{
@@ -20,23 +34,25 @@ public class SdlcAssOne {
 			
 			Document doc = Jsoup.connect("https://www.gutenberg.org/files/1065/1065-h/1065-h.htm").get();
 			
-			// using the p tag to isolate the body of the poem
-			
+			// using the p tag to isolate the body of the poem			
 			Elements newsHeadlines = doc.select("p");
-			for (Element headline : newsHeadlines) {
+			for (Element headline : newsHeadlines)
+			{
 				text += " " + headline.text();
 			}
 			// using the h1 tag to isolate the title of the poem
 			newsHeadlines = doc.select("h1");
 			for (Element headline : newsHeadlines) {
 				text += " " + headline.text();
+
 			}
 			// using the h2 tag to isolate the author
 			newsHeadlines = doc.select("h2");
 			for (Element headline : newsHeadlines) {
 				text += " " + headline.text();
-			}
 
+			}
+			
 		}
 		catch(IOException ex)
 		{
@@ -44,13 +60,13 @@ public class SdlcAssOne {
 		}
 		// cleaning the data to get an accurate word count by
 		// removing punctuation and changing everything to lowercase 
-		
+	
 		String[] textArray = text.toLowerCase().split("\\W+");  	
 
 		// creating three collections 
 		// 2 hashmaps - one to get frequency of each word - one to hold the sorted words
 		// 1 list to do the sorting/ranking top to bottom
-		
+
 		ArrayList<Integer> list = new ArrayList<>();
 	    Map<String, Integer> freqMap = new HashMap<>();
 	    LinkedHashMap<String, Integer> sortedMap = new LinkedHashMap<>();
